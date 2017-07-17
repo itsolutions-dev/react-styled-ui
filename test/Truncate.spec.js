d2dbd4355c166c1596d4bb00cd563a1d64de068f
Matteo Basso <mbasso96@gmail.com>
2017-07-17 16:28:42 +0200

    Truncate, tests

--- test/Truncate.spec.js
+++ test/Truncate.spec.js
@@ -6,11 +6,17 @@ import Truncate from '../src/Truncate';
 describe('Truncate', () => {
   test('should render a Truncate', () => {
     const component = ReactTestRenderer.create(<Truncate />);
-    expect(component).toMatchStyledComponentsSnapshot();
     expect(component).toBeDefined();
   });
 
   test('should have a p tag', () => {
     expect(Truncate).toHaveComponent('p');
+  });
+
+  test('should have correct styles', () => {
+    const component = ReactTestRenderer.create(<Truncate />);
+    expect(component).toHaveStyleRule('overflow', 'hidden');
+    expect(component).toHaveStyleRule('white-space', 'nowrap');
+    expect(component).toHaveStyleRule('text-overflow', 'ellipsis');
   });
 });
