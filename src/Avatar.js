4d666eab93000e75e2c9ab740022163e7f12ea11
DESKTOP-8G4245B\Federico <bottoni.fede@yahoo.it>
2017-10-20 12:32:30 +0200

    Avatar, added the way to render a not-img-node inside the component

--- src/Avatar.js
+++ src/Avatar.js
@@ -1,16 +1,47 @@
+// @flow
+
+import React from 'react';
 import styled, { css } from './styledComponent';
 import {
   getSizeOrDefault,
 } from './utils/';
 
 const defaultSize = '40px';
+const defaultRadius = '99999px';
+
+type AvatarProps = {
+  src: string,
+  alt: string,
+  children: any,
+};
 
 const Avatar = styled({
-  component: 'img',
-  style: css`
-    width: ${props => getSizeOrDefault(props.size, defaultSize)};
-    height: ${props => getSizeOrDefault(props.size, defaultSize)};
-    border-radius: ${props => getSizeOrDefault(props.radius, '99999px')};
+  component: 'div',
+  props: {
+    children: ({ src, alt, children }: AvatarProps) => (
+      src == null ? children : <img src={src} alt={alt || src} />
+    ),
+  },
+  style: css`    
+    ${props => (props.src == null ? `
+      width: ${getSizeOrDefault(props.size, defaultSize)};
+      height: ${getSizeOrDefault(props.size, defaultSize)};
+      border-radius: ${getSizeOrDefault(props.radius, defaultRadius)}; 
+      color: ${(props.color == null ? 'rgb(255, 255, 255)' : props.color)};
+      background-color: ${(props.backgroundColor == null ? 'rgb(188, 188, 188)' : props.backgroundColor)} !important;
+      font-size: ${getSizeOrDefault(props.fontSize, '20px')};
+      user-select: none;
+      align-items: center;
+      justify-content: center;
+      display: inline-flex;
+    ` : `
+      > img {
+        width: ${getSizeOrDefault(props.size, defaultSize)};
+        height: ${getSizeOrDefault(props.size, defaultSize)};
+        border-radius: ${getSizeOrDefault(props.radius, defaultRadius)};
+        display: inline-flex;
+      } 
+    `)} 
   `,
 });
 
--- test/Avatar.spec.js
+++ test/Avatar.spec.js
@@ -5,65 +5,194 @@ import theme from '../src/theme';
 import Avatar from '../src/Avatar';
 
 describe('Avatar', () => {
-  test('should render a Avatar', () => {
-    const component = ReactTestRenderer.create(<Avatar theme={theme} />);
+  test('should render a Img-Avatar', () => {
+    const component = ReactTestRenderer.create(<Avatar src="http://lorempixel.com/400/200/nature/" theme={theme} />);
     expect(component).toBeDefined();
     expect(component.toJSON()).toMatchSnapshot();
   });
 
-  test('should have an img tag', () => {
-    const component = ReactTestRenderer.create(<Avatar theme={theme} />).toJSON();
-    expect(component.type).toEqual('img');
+  test('should have an img tag [IMG]', () => {
+    const component = ReactTestRenderer.create(<Avatar src="http://lorempixel.com/400/200/nature/" theme={theme} />).toJSON();
+    expect(component.children[0].type).toEqual('img');
   });
 
-  test('should have a default radius', () => {
+  test('should have a default radius [IMG]', () => {
     const component = ReactTestRenderer.create(
       <Avatar
+        src="http://lorempixel.com/400/200/nature/"
         theme={theme}
       />,
     );
-    expect(component).toHaveStyleRule('border-radius', '99999px');
+    expect({
+      component,
+      modifier: '> img',
+    }).toHaveStyleRule('border-radius', '99999px');
   });
 
-  test('should have a radius', () => {
+  test('should have a radius [IMG]', () => {
     const component = ReactTestRenderer.create(
       <Avatar
+        src="http://lorempixel.com/400/200/nature/"
         theme={theme}
         radius={40}
       />,
     );
-    expect(component).toHaveStyleRule('border-radius', '40px');
+    expect({
+      component,
+      modifier: '> img',
+    }).toHaveStyleRule('border-radius', '40px');
   });
 
-  test('should have a default size', () => {
+  test('should have a default size [IMG]', () => {
+    const component = ReactTestRenderer.create(
+      <Avatar
+        src="http://lorempixel.com/400/200/nature/"
+        theme={theme}
+      />,
+    );
+    expect(({
+      component,
+      modifier: '> img',
+    })).toHaveStyleRule('height', '40px');
+    expect(({
+      component,
+      modifier: '> img',
+    })).toHaveStyleRule('width', '40px');
+  });
+
+  test('should handle string size [IMG]', () => {
+    const component = ReactTestRenderer.create(
+      <Avatar
+        src="http://lorempixel.com/400/200/nature/"
+        theme={theme}
+        size="20em"
+      />,
+    );
+    expect(({
+      component,
+      modifier: '> img',
+    })).toHaveStyleRule('height', '20em');
+    expect(({
+      component,
+      modifier: '> img',
+    })).toHaveStyleRule('width', '20em');
+  });
+
+  test('should handle numeric size [IMG]', () => {
+    const component = ReactTestRenderer.create(
+      <Avatar
+        src="http://lorempixel.com/400/200/nature/"
+        theme={theme}
+        size={30}
+      />,
+    );
+    expect(({
+      component,
+      modifier: '> img',
+    })).toHaveStyleRule('height', '30px');
+    expect(({
+      component,
+      modifier: '> img',
+    })).toHaveStyleRule('width', '30px');
+  });
+
+  test('should render a Avatar [TXT]', () => {
+    const component = ReactTestRenderer.create(<Avatar theme={theme} >A</Avatar>);
+    expect(component).toBeDefined();
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  test('should have a div tag [TXT]', () => {
+    const component = ReactTestRenderer.create(<Avatar theme={theme} >A</Avatar>).toJSON();
+    expect(component.type).toEqual('div');
+  });
+
+  test('should have a default radius [TXT]', () => {
+    const component = ReactTestRenderer.create(
+      <Avatar theme={theme}>A</Avatar>,
+    );
+    expect(component).toHaveStyleRule('border-radius', '99999px');
+  });
+
+  test('should have a radius [TXT]', () => {
     const component = ReactTestRenderer.create(
       <Avatar
         theme={theme}
-      />,
+        radius={40}
+      >
+        A
+      </Avatar>,
+    );
+    expect(component).toHaveStyleRule('border-radius', '40px');
+  });
+
+  test('should have a default size [TXT]', () => {
+    const component = ReactTestRenderer.create(
+      <Avatar theme={theme}>A</Avatar>,
     );
     expect(component).toHaveStyleRule('height', '40px');
     expect(component).toHaveStyleRule('width', '40px');
   });
 
-  test('should handle string size', () => {
+  test('should handle string size [TXT]', () => {
     const component = ReactTestRenderer.create(
       <Avatar
         theme={theme}
         size="20em"
-      />,
+      >
+        A
+      </Avatar>,
     );
     expect(component).toHaveStyleRule('height', '20em');
     expect(component).toHaveStyleRule('width', '20em');
   });
 
-  test('should handle numeric size', () => {
+  test('should handle numeric size [TXT]', () => {
     const component = ReactTestRenderer.create(
       <Avatar
         theme={theme}
         size={30}
-      />,
+      >
+        A
+      </Avatar>,
     );
     expect(component).toHaveStyleRule('height', '30px');
     expect(component).toHaveStyleRule('width', '30px');
+  });
+
+  test('should have color [TXT]', () => {
+    const component = ReactTestRenderer.create(
+      <Avatar
+        theme={theme}
+        color="#FF0000"
+      >
+        A
+      </Avatar>,
+    );
+    expect(component).toHaveStyleRule('color', '#FF0000');
+  });
+
+  test('should have background-color [TXT]', () => {
+    const component = ReactTestRenderer.create(
+      <Avatar
+        theme={theme}
+        backgroundColor="#FF0000"
+      >
+        A
+      </Avatar>,
+    );
+    expect(component).toHaveStyleRule('background-color', '#FF0000 !important');
+  });
+
+  test('should match snapshot', () => {
+    const component = ReactTestRenderer.create(
+      <Avatar
+        theme={theme}
+        position="absolute"
+      >
+        A
+      </Avatar>,
+    );
+    expect(component).toMatchSnapshot();
   });
 });
